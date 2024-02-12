30b1229ebb4fee2a4e6bde19c8408f473e375b0f
Delisma <delispence7@gmail.com>
2024-02-12 18:02:30 -0500

    added submit button

--- index.js
+++ index.js
@@ -63,3 +63,108 @@ document.getElementById('toggle-form').addEventListener('click', function() {
 // toggleButtonMenu();
 toggleButton();
 dataFetch();
+
+/*<form id="new-drink">
+  <label for="drink-name">Drink Name:</label>
+  <input type="text" id="drink-name" name="drink-name" required>
+  <label for="category">Category:</label>
+  <input type="text" id="category" name="category">
+  <input type="submit" id="submit-button" value="Submit">
+</form>
+
+
+  const form = document.getElementById('new-drink');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const drinkNameInput = document.getElementById('drink-name');
+    const drinkNameValue = drinkNameInput.value;
+
+    console.log(drinkNameValue);
+</form>
+  });
+  <form id="new-drink">
+  <label for="drink-name">Drink Name:</label>
+  <input type="text" id="drink-name" name="drink-name" required>
+  <label for="category">Category:</label>
+  <input type="text" id="category" name="category">
+  <input type="submit" id="submit-button" value="Submit">
+</form>
+*/
+const addSubmitListener = (event) => {
+  event.preventDefault();
+  let newDrink = {
+    idDrink:  '',
+    strDrink: event.target[0].value,
+    strCategory: event.target[1].value,
+    strAlcoholic: event.target[2].value,
+    strGlass: event.target[3].value,
+    strInstructions: event.target[4].value,
+    strInstructionsDE:'',
+    strInstructionsIT:'',
+    strDrinkThumb: event.target[5].value,
+    strIngredient1: event.target[6].value,
+    strIngredient2: event.target[7].value,
+    strIngredient3: event.target[8].value,
+    strIngredient4: event.target[9].value,
+    strMeasure1: event.target[10].value,
+    strMeasure2: event.target[11].value,
+    strCreativeCommonsConfirmed:'',
+    dateModified:''
+  };
+  fetch('http://localhost:3000/drinks', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newDrink)
+  })
+  .then(response => {
+    if (response.status === 201) {
+      return response.json()
+    }
+  })
+  .then((data) => {
+const drinkFormName = document.getElementById('new-drink');  
+const drinkNameInput = document.getElementById('drink-name');
+const categoryInput = document.getElementById('category');
+const glassInput = document.getElementById('glass-type')
+const instructionsInput =document.getElementById('instructions')
+const drinkThumbInput = document.getElementById('new-image')
+const ingredient1Input = document.getElementById('ingredient-One')
+const ingredient2Input = document.getElementById('ingredient-two')
+const ingredient3Input = document.getElementById('ingredient-three')
+const Ingredient4Input = document.getElementById('ingredient-four')
+const measure1Input = document.getElementById('Mesurment-One')
+const measure2Input = document.getElementById('Mesurment-two')
+const measure3Input = document.getElementById('Mesurment-three')
+const measure4Input = document.getElementById('Mesurment-four')
+
+drinkNameInput.value  = newDrink.strDrink
+categoryInput.value  = newDrink.strCategory
+glassInput.value  = newDrink.strGlass
+instructionsInput.value = newDrink.strInstructions
+drinkThumbInput.value  = newDrink.strDrinkThumb
+ingredient1Input.value  = newDrink.strIngredient1
+ingredient2Input.value  = newDrink.strIngredient2
+ingredient3Input.value  = newDrink.strIngredient3
+Ingredient4Input.value  = newDrink.strIngredient4
+measure1Input.value  = newDrink.strMeasure1
+measure2Input.value  = newDrink.strMeasure2
+measure3Input.value  = newDrink.strMeasure3
+measure4Input.value  = newDrink.strMeasure4
+})
+};
+const form = document.getElementById('new-drink');
+
+form.addEventListener('submit', (event) =>{
+  addSubmitListener(event);
+  dataFetch();
+
+      // Create an object to hold the form data
+
+
+    // Send the form data to the server
+  })
+  
